f08ea158608203eb0074e61b1614618e06b28f92
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-22 23:16:42 +0200

    refactoring controllers to use asyncWrapper

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -24,21 +24,19 @@ const getAllTasks = async (req, res) => {
   */
 
 // get a single task
-const getTask = async (req, res) => {
+const getTask = asyncWrapper(async (req, res) => {
   // Using Model.findById() query
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findById(taskID);
-    if (!task) {
-      return res.status(404).json({ msg: `No task with id: ${taskID}` });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  const { id: taskID } = req.params;
+  const task = await Task.findById(taskID);
+  if (!task) {
+    return res.status(404).json({ msg: `No task with id: ${taskID}` });
   }
+  res.status(200).json({ task });
+});
 
-  /* 
+/* 
   // Using Model.findOne() query
+  const getTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
@@ -49,68 +47,53 @@ const getTask = async (req, res) => {
   } catch (error) {
     res.status(500).json({ msg: error });
   }
-   */
 };
+*/
 
 // create a new task
-const createTask = async (req, res) => {
-  try {
-    const taskToAdd = await Task.create(req.body);
-    res.status(201).json({ taskToAdd });
-  } catch (error) {
-    if ((error.errors.name.name = "ValidatorError")) {
-      const errorMessage = error.errors.name.properties.message;
-      res.status(500).json({ msg: errorMessage });
-    } else {
-      res.status(500).json({ msg: error });
-    }
-  }
-};
+const createTask = asyncWrapper(async (req, res) => {
+  const taskToAdd = await Task.create(req.body);
+  res.status(201).json({ taskToAdd });
+});
 
 // edit a single task (PUT)
-const editTask = async (req, res) => {
+const editTask = asyncWrapper(async (req, res) => {
   // Using Model.findOneAndReplace() query
-  try {
-    const { id: taskID } = req.params;
-    // Model.findOneAndReplace({conditions}, {replacement}, {options})
-    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-      // returnDocument: "after",
-      new: true,
-      runValidators: true,
-      overwrite: true,
+  const { id: taskID } = req.params;
+  // Model.findOneAndReplace({conditions}, {replacement}, {options})
+  const task = await Task.findOneAndReplace({ _id: taskID }, req.body, {
+    // returnDocument: "after",
+    new: true,
+    runValidators: true,
+    overwrite: true,
+  });
+  if (!task) {
+    return res.status(404).json({
+      msg: `No task with ID: ${taskID} found. Database hasn\'t been updated`,
     });
-    if (!task) {
-      return res.status(404).json({
-        msg: `No task with ID: ${taskID} found. Database hasn\'t been updated`,
-      });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ success: false, msg: error });
   }
-};
+  res.status(200).json({ task });
+});
 
 // update a single task (PATCH)
-const updateTask = async (req, res) => {
+const updateTask = asyncWrapper(async (req, res) => {
+  const { id: taskID } = req.params;
   // Using Model.findByIdAndUpdate() query
-  try {
-    const { id: taskID } = req.params;
-    // Model.findOneAndUpdate(id, {update}, {options})
-    const task = await Task.findByIdAndUpdate(taskID, req.body, {
-      new: true,
-      runValidators: true,
+  // Model.findByIdAndUpdate(id, {update}, {options})
+  const task = await Task.findByIdAndUpdate(taskID, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!task) {
+    return res.status(404).json({
+      msg: `No task with ID: ${taskID} found. Database hasn\'t been updated`,
     });
-    if (!task) {
-      return res.status(404).json({
-        msg: `No task with ID: ${taskID} found. Database hasn\'t been updated`,
-      });
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
   }
+  res.status(200).json({ task });
+});
 
-  /* 
+/* 
+const updateTask = async (req, res) => {
   // Using Model.findOneAndUpdate() query
   try {
     const { id: taskID } = req.params;
@@ -129,26 +112,24 @@ const updateTask = async (req, res) => {
   } catch (error) {
     res.status(500).json({ success: false, msg: error });
   }
-   */
 };
+ */
 
 // delete a single task
-const deleteTask = async (req, res) => {
+const deleteTask = asyncWrapper(async (req, res) => {
   // Using Model.findByIdAndDelete() query
-  try {
-    const { id: taskID } = req.params;
-    const task = await Task.findByIdAndDelete(taskID);
-    if (!task) {
-      return res
-        .status(404)
-        .json(`No task with id: ${taskID} was found. Not deleted.`);
-    }
-    res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
+  const { id: taskID } = req.params;
+  const task = await Task.findByIdAndDelete(taskID);
+  if (!task) {
+    return res
+      .status(404)
+      .json(`No task with id: ${taskID} was found. Not deleted.`);
   }
+  res.status(200).json({ task });
+});
 
-  /* 
+/* 
+const ddeleteTask = async (req, res) => {
   // Using Model.findOneAndDelete() query
   try {
     const { id: taskID } = req.params;
@@ -162,8 +143,8 @@ const deleteTask = async (req, res) => {
   } catch (error) {
     res.status(500).json({ msg: error });
   }
-   */
 };
+ */
 
 module.exports = {
   getAllTasks,
