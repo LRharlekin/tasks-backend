1291669fb72efdb0dee1a176e70283b5f5f00a31
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-22 15:44:52 +0200

    comments / static middleware added

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const connectDB = require("./db/connect");
 require("dotenv").config();
 
 // middleware
+// app.use(express.static('./public'));
 app.use(express.json());
 
+// routes
 app.get("/", (req, res) => {
   res.status(200).send("hello world.");
 });
