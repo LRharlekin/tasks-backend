4dfe5d01d9ba19edd622db6e8536cadd3d7f440e
LRharlekin <13718192+LRharlekin@users.noreply.github.com>
2023-12-22 14:59:02 +0200

    GET and DELETE controller methods implemented

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -16,12 +16,32 @@ const getAllTasks = async (req, res) => {
 };
 
 // get a single task
-const getTask = (req, res) => {
-  // const task = db.find((task) => task.id === Number(req.params.id));
-  // if (!task) {
-  //   return res.status(400).send(`task with id ${req.params.id} wasn't found.`);
-  // }
-  res.status(200).send(`get single task with ID: ${req.params.id}`);
+const getTask = async (req, res) => {
+  // Using Model.findById() query
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findById(taskID);
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
+  /* 
+  // Using Model.findOne() query
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+   */
 };
 
 // create a new task
@@ -43,9 +63,38 @@ const createTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send(`update single task with ID: ${req.params.id}`);
 };
+
 // delete a single task
-const deleteTask = (req, res) => {
-  res.send(`delete single task with ID: ${req.params.id}`);
+const deleteTask = async (req, res) => {
+  // Using Model.findByIdAndDelete() query
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndDelete(taskID);
+    if (!task) {
+      return res
+        .status(404)
+        .json(`No task with id: ${taskID} was found. Not deleted.`);
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+
+  /* 
+  // Using Model.findOneAndDelete() query
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res
+        .status(404)
+        .json(`No task with id: ${taskID} was found. Not deleted.`);
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+   */
 };
 
 module.exports = {
